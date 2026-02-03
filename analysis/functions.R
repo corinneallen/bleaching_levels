05a04f4b5393331d621e79f6bba93e003052c6e5
corinneallen <65238969+corinneallen@users.noreply.github.com>
2026-02-03 17:19:53 -0500

    Add files via upload
    Signed-off-by: corinneallen <65238969+corinneallen@users.noreply.github.com>

---
+++ analysis/functions.R
@@ -0,0 +1,17 @@
+read_fasta_df <- function (file = "") {
+  fasta <- readLines(file)
+  ind <- grep(">", fasta)
+  s <- data.frame(ind = ind, from = ind + 1, to = c((ind - 1)[-1], length(fasta)))
+  seqs <- rep(NA, length(ind))
+  for (i in 1:length(ind)) {
+    seqs[i] <- paste(fasta[s$from[i]:s$to[i]], collapse = "")
+  }
+  tib <- tibble(name = gsub(">", "", fasta[ind]), sequence = seqs)
+  return(tib)
+}
+
+dna_to_DNAbin <- function (dna){
+  DNAbin <- as_DNAbin(dna)
+  names(DNAbin) <- names(dna)
+  return(DNAbin)
+}
